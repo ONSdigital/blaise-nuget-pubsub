68cc5c4d4cf2615787a32e375f3361965a0e89db
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-08-17 16:46:17 +0100

    Added snapshot service class

---
+++ Blaise.Nuget.PubSub.Core/Services/SnapshotService.cs
@@ -0,0 +1,14 @@
+﻿using Google.Cloud.PubSub.V1;
+
+namespace Blaise.Nuget.PubSub.Core.Services
+{
+    public class SnapshotService
+    {
+        private SubscriberServiceApiClient _subscriberServiceClient;
+
+        public SnapshotService(SubscriberServiceApiClient subscriberServiceClient)
+        {
+            _subscriberServiceClient = subscriberServiceClient;
+        }
+    }
+}
